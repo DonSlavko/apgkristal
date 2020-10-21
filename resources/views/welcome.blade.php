8596fa5baff0494d6ac50b6891675629b720e1bf
Slavko Lazic <lazic.slavko.91@gmail.com>
2020-10-21 19:49:42 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>APG Kristal group</title>
+    <meta content="" name="descriptions">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: Regna - v2.1.0
+    * Template URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+<header id="header" class="header-transparent">
+    <div class="container">
+
+        <div id="logo" class="pull-left">
+            <!--<a href="index.html">KRISTAL&lt;!&ndash;<img src="assets/img/logo.png" alt="">&ndash;&gt;</a>-->
+            <!-- Uncomment below if you prefer to use a text logo -->
+            <h1><a href="#hero">Kristal</a></h1>
+        </div>
+
+        <nav id="nav-menu-container">
+            <ul class="nav-menu">
+                <li class="menu-active"><a href="#hero">Home</a></li>
+                <li><a href="#about">O nama</a></li>
+                <li><a href="#services">Usluge</a></li>
+                <li><a href="#portfolio">Galerija</a></li>
+                <li><a href="#contact">Kontakt</a></li>
+            </ul>
+        </nav><!-- #nav-menu-container -->
+    </div>
+</header><!-- End Header -->
+
+<!-- ======= Hero Section ======= -->
+<section id="hero">
+    <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
+
+        <!--<h1>Welcome to Regna</h1>
+        <h2>We are team of talanted designers making websites with Bootstrap</h2>
+        <a href="#about" class="btn-get-started">Get Started</a>-->
+    </div>
+</section><!-- End Hero Section -->
+
+<main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about">
+        <div class="container" data-aos="fade-up">
+            <div class="row about-container">
+
+                <div class="col-lg-6 content order-lg-1 order-2">
+                    <h2 class="title"> A.P.G Kristal group je</h2>
+                    <p>društvo za proizvodnju, trgovinu i usluge, osnovano 2008 godine u Beogradu sa osnovnom delatnošću:
+                        uvoz, proizvodnja, pakovanje i distribucija svih vrsta kuhinjskih folija kako za upotrebu u
+                        domaćinstvima tako i za profesionalnu upotrebu u ugostiteljskim objektima.</p>
+
+                    <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+                        <div class="icon"><i class="fa fa-shopping-bag"></i></div>
+                        <h4 class="title"><a href="">O nama</a></h4>
+                        <p class="description">Za 12 godina iskustva i kontinuiranog razvoja stali smo rame uz rame sa
+                            liderima u branši pa tako sada robu i proizvode plasiramo širom države uključujući i velike
+                            trgovinske lance ali i, uz čvrstu saradnju u zemlje regiona. Konstantnim inovacijama i
+                            ulaganjem  u razvoj kako kadrova tako i proizvodnih kapaciteta a u isto vreme poštujući
+                            principe i interese tržišta timskim radom omogućavamo kreativnu i efikasnu realizaciju
+                            poslovnih zadataka a tako nastavljamo da osiguravamo vrhunski kvalitet proizvoda i
+                            usluga  koje imamo u svom asortimanu.</p>
+                    </div>
+
+                    <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+                        <div class="icon"><i class="fa fa-photo"></i></div>
+                        <h4 class="title"><a href="">Upotreba</a></h4>
+                        <p class="description">Aluminijumska folija koristi se za proizvodnju aseptičnih, dugotrajnih
+                            pakovanja pića i mlečnih proizvoda, prilikom pečenja, pakovanja i prenošenja hrane.
+                            Takođe, alu folija je široko rasporostranjena u procesima toplotne izolacije, a u posebnim
+                            legurama se koristi čak i za strukturne komponente aviona. Rasprostranjena je u
+                            najrazličitijim segmentima savremenog života. Kritikovana je od strane nekih organizacija
+                            za zaštitu životne sredine zbog visoke cene i energije koja se troši u procesu proizodnje,
+                            međutim, modernim procesima reciklaže negativne komponente se smanjuju.
+                        </p>
+                    </div>
+
+                    <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+                        <div class="icon"><i class="fa fa-bar-chart"></i></div>
+                        <h4 class="title"><a href="">Nezaobilazna u modernoj kuhinji</a></h4>
+                        <p class="description">Upotreba aluminijumske folije je ipak najrasprostranjenija u domaćinstvu.
+                            U modernoj kuhinji danas se gotovo i ne može bez aluminijumske folije, zbog toga što se
+                            može koristiti za kuvanje, čuvanje i prenošenje hrane. Idealna je za: kuvanje i podgrevanje
+                            hrane u rerni, održavanje toplote hrane, zaštitu hrane od spoljašnjih nečistoća, pripremanje
+                            zdravih namirnica – ribe ili mesa koji se umotani u alu foliju kuvaju u svom soku bez
+                            dodataka ulja. Takođe se može koristiti i za čuvanje hrane u frižideru ili zamrzivaču,
+                            kao i za prenošenje hrane.</p>
+                    </div>
+
+                </div>
+
+                <div class="col-lg-6 background order-lg-2 order-1" data-aos="fade-left" data-aos-delay="100"></div>
+            </div>
+
+        </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services">
+        <div class="container" data-aos="fade-up">
+            <div class="section-header">
+                <h3 class="section-title">Usluge</h3>
+                <p class="section-description">Osiguravamo vrhunski kvalitet proizvoda I usluga  koje imamo u svom asortimanu.
+                    Najznačajnije mesto u ponudi asortimana zauzimaju: aluminijumska folija, streč – pvc folija,
+                    papir za pečenje i aluminijumske posude – kontejneri za pakovanje hrane.</p>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+                    <div class="box">
+                        <div class="icon"><a href=""><i class="fa fa-desktop"></i></a></div>
+                        <h4 class="title"><a href="">Aluminijumska folija</a></h4>
+                        <p class="description">Aluminijumska folija je odličan saveznik u svakoj kuhinji. Često se koristi prilikom pečenja, ali i kod zamrzavanja namirnica.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+                    <div class="box">
+                        <div class="icon"><a href=""><i class="fa fa-bar-chart"></i></a></div>
+                        <h4 class="title"><a href="">Prijanjajuća folija</a></h4>
+                        <p class="description">Primena prijanjajuće folije veže se uz prehrambenu industriju, pića, farmaceutsku industriju, supermarkete, poljoprivredu i gotovo svim drugim industrijskim granama.</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6" data-aos="zoom-in">
+                    <div class="box">
+                        <div class="icon"><a href=""><i class="fa fa-paper-plane"></i></a></div>
+                        <h4 class="title"><a href="">Papir za pečenje</a></h4>
+                        <p class="description">Papir za pečenje je vrlo praktična stvar, njegova je glavna svrha zaštititi tavu od paljenja i pomoći domaćici da brže rešava kuhinjske poslove.</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Call To Action Section ======= -->
+    <section id="call-to-action">
+    </section>
+    <!-- End Call To Action Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+        <div class="container" data-aos="fade-up">
+            <div class="section-header">
+                <h3 class="section-title">Galerija</h3>
+                <p class="section-description">Iz asortimana proizvoda za domaćinstvo izdvajamo aluminijumsku foliju, streč foliju i papir za pečenje, u pojedinačnim pakovanjima različitih dimenzija u zavisnosti od potreba kupaca…</p>
+            </div>
+
+            <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+                <div class="col-lg-4 col-md-6 portfolio-item">
+                    <div class="owl-carousel portfolio-details-carousel">
+                        <img src="assets/img/portfolio/alu-1.png" class="img-fluid" alt="Aluminijumska folija">
+                        <img src="assets/img/portfolio/alu-2.png" class="img-fluid" alt="Aluminijumska folija">
+                        <img src="assets/img/portfolio/alu-3.png" class="img-fluid" alt="Aluminijumska folija">
+                        <img src="assets/img/portfolio/alu-4.png" class="img-fluid" alt="Aluminijumska folija">
+                    </div>
+                    <div class="portfolio-info">
+                        <h4>Aluminijumska folija</h4>
+                        <p></p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item">
+                    <div class="owl-carousel portfolio-details-carousel">
+                        <img src="assets/img/portfolio/fol-1.png" class="img-fluid" alt="Prijanjajuća folija">
+                        <img src="assets/img/portfolio/fol-2.png" class="img-fluid" alt="Prijanjajuća folija">
+                        <img src="assets/img/portfolio/fol-3.png" class="img-fluid" alt="Prijanjajuća folija">
+                        <img src="assets/img/portfolio/fol-4.png" class="img-fluid" alt="Prijanjajuća folija">
+                    </div>
+                    <div class="portfolio-info">
+                        <h4>Prijanjajuća folija</h4>
+                        <p></p>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 portfolio-item">
+                    <div class="owl-carousel portfolio-details-carousel">
+                        <img src="assets/img/portfolio/pap-1.png" class="img-fluid" alt="Papir za pečenje">
+                        <img src="assets/img/portfolio/pap-2.png" class="img-fluid" alt="Papir za pečenje">
+                        <img src="assets/img/portfolio/pap-3.png" class="img-fluid" alt="Papir za pečenje">
+                        <img src="assets/img/portfolio/pap-4.png" class="img-fluid" alt="Papir za pečenje">
+                    </div>
+                    <div class="portfolio-info">
+                        <h4>Papir za pečenje</h4>
+                        <p></p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!-- End Portfolio Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact">
+        <div class="container">
+            <div class="section-header">
+                <h3 class="section-title">Kontakt</h3>
+                <p class="section-description"></p>
+            </div>
+        </div>
+
+        <!-- Uncomment below if you wan to use dynamic maps -->
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2831.234869541755!2d20.519683815497796!3d44.79640187909881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a7a745bd7ed5f%3A0xd0ae6b1b2cd77959!2z0IjQvtCy0LDQvdC60LUg0KDQsNC00LDQutC-0LLQuNGbIDI5LCDQkdC10L7Qs9GA0LDQtA!5e0!3m2!1ssr!2srs!4v1578168178457!5m2!1ssr!2srs" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+
+                <div class="col-lg-3 col-md-4">
+
+                    <div class="info">
+                        <div>
+                            <i class="fa fa-map-marker"></i>
+                            <p>Jovanke Radaković 29b,<br>11000 Beograd, Srbija</p>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-envelope"></i>
+                            <p>apgkristalgroup@gmail.com</p>
+                        </div>
+
+                        <div>
+                            <i class="fa fa-phone"></i>
+                            <p>
+                                tel/fax : 011/34-24-145 <br>
+                                direktor : 060/50-65-566 <br>
+                                office 1 : 060/50-65-588 <br>
+                                office 2 : 060/37-56-186 <br>
+
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="col-lg-5 col-md-8">
+                    <div class="form">
+                        <form action="/" method="post" role="form" class="php-email-form">
+                            <div class="form-group">
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Ime i Prezime" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                <div class="validate"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Email" data-rule="email" data-msg="Please enter a valid email" />
+                                <div class="validate"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Tema" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                <div class="validate"></div>
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Poruka" placeholder="Message"></textarea>
+                                <div class="validate"></div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="loading">Loading</div>
+                                <div class="error-message"></div>
+                                <div class="sent-message">Your message has been sent. Thank you!</div>
+                            </div>
+                            <div class="text-center"><button type="submit">Send Message</button></div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <!-- End Contact Section -->
+
+</main>
+<!-- End #main -->
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong>APG Kristal group</strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            Made by <a href="https://stcompany.org">ST Company</a>
+            <!--
+            All the links in the footer should remain intact.
+            You can delete the links only if you purchased the pro version.
+            Licensing information: https://bootstrapmade.com/license/
+            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Regna
+          -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
+
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/counterup/counterup.min.js"></script>
+<script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/superfish/superfish.min.js"></script>
+<script src="assets/vendor/hoverIntent/hoverIntent.js"></script>
+<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="assets/vendor/venobox/venobox.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
